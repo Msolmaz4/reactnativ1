542d35cebb83bc1befd56003b10c73740ffbe71f
mustafa <msolmaz83@gmail.com>
2023-12-12 01:15:44 +0100

    useState

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput,Button} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [data,setData] = useState('')
+ const textHand = (ver)=>{
+setData(ver)
+ }
+ const buttonHan = ()=>{
+console.log(data)
+ }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>denmeler erererer</Text>
+ <TextInput placeholder='adiniyi giriniy' onChangeText={textHand}/>
+ <Button title='Ekle' onPress={buttonHan}></Button>
       <StatusBar style="auto" />
     </View>
   );
