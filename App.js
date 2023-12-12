a768fa80b4f4b069e800f89c5ee871ffb3474782
mustafa <msolmaz83@gmail.com>
2023-12-12 01:52:45 +0100

    todo

--- App.js
+++ App.js
@@ -4,18 +4,25 @@ import { useState } from 'react';
 
 export default function App() {
   const [data,setData] = useState('')
+  const [todo,setTodo] = useState([])
  const textHand = (ver)=>{
 setData(ver)
  }
  const buttonHan = ()=>{
-console.log(data)
+setTodo([...todo,data])
+setData('')
  }
  
   return (
     <View style={styles.container}>
-      <Text>denmeler erererer</Text>
+      <Text>denmeler ererereeeeeeeer</Text>
  <TextInput placeholder='adiniyi giriniy' onChangeText={textHand}/>
  <Button title='Ekle' onPress={buttonHan}></Button>
+ {
+todo?.map((item)=>(
+  <Text key={item}>{item}</Text>
+))
+ }
       <StatusBar style="auto" />
     </View>
   );
